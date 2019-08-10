c8855cdeca4579032c9fd0be8395e652604d156d
Achraf Faddoul <afaddoul@e1r5p12.1337.ma>
2019-08-10 22:34:55 +0100

    player and map dimensions parsed ; no leaks

--- main.c
+++ main.c
@@ -6,32 +6,63 @@
 /*   By: afaddoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 16:33:20 by afaddoul          #+#    #+#             */
-/*   Updated: 2019/08/10 15:46:04 by afaddoul         ###   ########.fr       */
+/*   Updated: 2019/08/10 22:34:18 by afaddoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int		get_player(void)
+void 	free_two_dim_arr(char **arr, int size)
 {
-	int player;
+	int 	i;
+
+	i = 0;
+	while (size)
+	{
+		free(arr[i]);
+		i++;
+		size--;
+	}
+	free(arr);
+}
+
+void	get_player(int *p)
+{
 	char **arr;
 	char *line;
 
 	get_next_line(0, &line);
 	arr = ft_strsplit(line, ' ');
-	player = ft_atoi(arr[2] + 1);
-	free(arr);
+	*p = ft_atoi(arr[2] + 1);
+	free_two_dim_arr(arr, 5);
 	free(line);
-	return (player);
 }
-/// $$$ exec p12  : [PLAYER_NAME]
+
+void 	get_dimensions(int *x, int *y)
+{
+	char	**arr;
+	char 	*line;
+
+	get_next_line(0, &line);
+	arr = ft_strsplit(line, ' ');
+	*x = ft_atoi(arr[1]);
+	*y = ft_atoi(arr[2]);
+	free_two_dim_arr(arr, 3);
+	free(line);
+}
+
 int 	main()
 {
 	int player;
+	int x;
+	int y;
 
-	player = get_player();
-	printf("|||%d|||\n", player);
+	player = 1;
+	x = 0;
+	y = 0;
+	get_player(&player);
+	get_dimensions(&x, &y);
+	printf("%d\n%d\n%d\n", player, x, y);
 	return (0);
 }
