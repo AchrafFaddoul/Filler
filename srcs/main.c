7942b2c1fc7af789718bfedcdddb0b9b3425c1e1
Achraf Faddoul <afaddoul@e1r8p13.1337.ma>
2019-08-13 15:03:05 +0100

    hit map created

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: afaddoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 16:33:20 by afaddoul          #+#    #+#             */
-/*   Updated: 2019/08/11 20:47:31 by afaddoul         ###   ########.fr       */
+/*   Updated: 2019/08/13 15:02:45 by afaddoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,7 +123,7 @@ char 		**create_board(int x, int y)
 	int 	i;
 
 	i = 0;
-	board = ft_memalloc(sizeof(char **) * (x + 1));
+	board = ft_memalloc(sizeof(char *) * (x + 1));
 	board[x + 1] = 0;
 	while (x)
 	{
@@ -161,7 +161,7 @@ char 		**create_token(int x, int y)
 	int 	i;
 
 	i = 0;
-	token = ft_memalloc(sizeof(char **) * (x + 1));
+	token = ft_memalloc(sizeof(char *) * (x + 1));
 	token[x + 1] = 0;
 	while (x)
 	{
@@ -223,10 +223,67 @@ int			get_dim_token(int *x, int *y)
 	return (1);
 }
 
-int 			main()
+int 			**create_h_map(int x, int y)
+{
+	int 		**h_map;
+	int 		i;
+
+	i = 0;
+	h_map = ft_memalloc(sizeof(int *) * x);
+	while (x)
+	{
+		h_map[i] = ft_memalloc(sizeof(int) * y);
+		i++;
+		x--;
+	}
+	return (h_map);
+}
+
+void			init_h_map(int **h_map, char **board)
+{
+	int 		i;
+	int 		j;
+
+	i = 0;
+	j = 0;
+	while (board[i])
+	{
+		j = 0;
+		while (board[i][j])
+		{
+			if (board[i][j] == 'o' || board[i][j] == 'O')
+				h_map[i][j] = -1;
+			else if (board[i][j] == 'x' || board[i][j] == 'X')
+				h_map[i][j] = -2;
+			j++;
+
+		}
+		i++;
+	}
+}
+/*
+void 			init_target(int **h_map, int target)
+{
+	while ()
+}
+
+void 			fill_h_map(int **h_map, int player)
+{
+	int 		target;
+
+	target = 0;
+	if (player == 1)
+		target = -2;
+	else if (player == 2)
+		target = -1;
+	init_to_zero(h_map, target);
+}
+*/
+int 			main(void)
 {
 	t_board		board;
 	t_token		token;
+	int 		**h_map;
 	int 		player;
 	int 		check;
 
@@ -238,18 +295,52 @@ int 			main()
 	while (1)
 	{
 		check = get_dim_board(&(board.x), &(board.y));
-		if (check == -1)
-			return (0);
 		board.arr = fill_board(board.x, board.y);
-		check = get_dim_token(&(token.x), &(token.y));
 		if (check == -1)
 			return (0);
+		check = get_dim_token(&(token.x), &(token.y));
 		token.piece = fill_token(token.x, token.y);
+
+		if (check == -1)
+			return (0);
 		/*
 		 *algorithm
 		 */
+		h_map = create_h_map(board.x, board.y);
+		init_h_map(h_map, board.arr);
+	//	fill_h_map(h_map, player);
 		free_two_dim_arr(board.arr, board.x);
 		free_two_dim_arr(token.piece, token.x);
+		break ;
 	}
 	return (0);
 }
+
+/*
+		int i = 0;
+		int j = 0;
+		while (i < board.x)
+		{
+			j = 0;
+			while (j < board.y)
+			{
+				printf("%d", h_map[i][j]);
+				j++;
+			}
+			printf("\n");
+			i++;
+		}
+		i = 0;
+		j = 0;
+		while (i < board.x)
+		{
+			j = 0;
+			while (j < board.y)
+			{
+				printf("%c", board.arr[i][j]);
+				j++;
+			}
+			printf("\n");
+			i++;
+		}
+ */
