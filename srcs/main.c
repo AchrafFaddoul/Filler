cb2abb93a6b973d37a1ba35f76ac0c3fdf7f68d2
afaddoul <faddoulachraf39@gmail.com>
2019-08-30 16:04:53 +0100

    main are optimised

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,46 @@
 /*   By: afaddoul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/07 16:33:20 by afaddoul          #+#    #+#             */
-/*   Updated: 2019/08/28 19:33:23 by afaddoul         ###   ########.fr       */
+/*   Updated: 2019/08/29 15:50:36 by afaddoul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/filler.h"
+
+static void		ft_putresult(int x, int y)
+{
+	ft_putnbr(x);
+	ft_putstr(" ");
+	ft_putnbr(y);
+	ft_putstr("\n");
+}
+
+static int		ft_initdata(t_board *board, t_token *token)
+{
+	if (get_dim_board(&board->x, &board->y) == -1)
+		return (0);
+	if (!(board->arr = fill_board(board->x, board->y)))
+		return (0);
+	if (get_dim_token(&token->x, &token->y) == -1)
+		return (0);
+	if (!(token->piece = fill_token(token->x, token->y)))
+	{
+		free_two_dim_arr(board->arr, board->x);
+		return (0);
+	}
+	return (1);
+}
+
+static int		ft_destroyall(t_game *game, t_pos *pos,
+					t_board *board, t_token *token)
+{
+	free(game);
+	free(pos);
+	free_two_dim_arr(board->arr, board->x);
+	free_two_dim_arr(token->piece, token->x);
+	free_int_two_dim_arr(board->h_map, board->x);
+	return (0);
+}
 
 int				main(void)
 {
@@ -18,54 +53,23 @@ int				main(void)
 	t_token		token;
 	t_game		*game;
 	t_pos		*pos;
-	int			player;
-	int			check;
 
-	player = 1;
-	check = 0;
-	check = get_player(&player);
-	if (check == -1)
+	board.player = 1;
+	if (get_player(&board.player) == -1)
 		return (0);
 	while (1)
 	{
-		check = get_dim_board(&(board.x), &(board.y));
-		if (check == -1)
+		if (ft_initdata(&board, &token) == 0)
 			return (0);
-		if (!(board.arr = fill_board(board.x, board.y)))
-			return (0);
-		check = get_dim_token(&(token.x), &(token.y));
-		if (check == -1)
-			return (0);
-		if (!(token.piece = fill_token(token.x, token.y)))
-		{
-			free_two_dim_arr(board.arr, board.x);
-			return (0);
-		}
-		board.player = player;
 		board.h_map = create_h_map(board.x, board.y);
 		init_h_map(board.h_map, board.arr);
-		fill_h_map(board.h_map, board, player);
+		fill_h_map(board.h_map, board, board.player);
 		if (!(pos = get_stars_pos_dispatcher(&token)))
-		{
-			free_two_dim_arr(board.arr, board.x);
-			free_two_dim_arr(token.piece, token.x);
-			free_int_two_dim_arr(board.h_map, board.x);
-			return (0);
-		}
+			return (ft_destroyall(NULL, NULL, &board, &token));
 		if (!(game = put_token(board, pos, &token)))
-		{
-			free(pos);
-			return (0);
-		}
-		ft_putnbr(game->x);
-		ft_putstr(" ");
-		ft_putnbr(game->y);
-		ft_putstr("\n");
-		free(game);
-		free(pos);
-		free_two_dim_arr(board.arr, board.x);
-		free_two_dim_arr(token.piece, token.x);
-		free_int_two_dim_arr(board.h_map, board.x);
+			return (ft_destroyall(NULL, pos, &board, &token));
+		ft_putresult(game->x, game->y);
+		ft_destroyall(game, pos, &board, &token);
 	}
 	return (0);
 }
